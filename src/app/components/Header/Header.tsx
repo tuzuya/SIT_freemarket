7b35e7e315ff2f527bf909f9ee0f49848a0e08cf
ItoTsuzuya <tuzuya1220@gmail.com>
2026-05-27 11:18:01 +0900

    FIX: ヘッダーの修正

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -15,20 +15,6 @@ export default function Header( {pageTitle, imgSrc}: HeaderProps ){
 
     return (
         <header className={styles.Header}>
-            <div className={styles.HeaderMeta}>
-                <time className={styles.HeaderTime}>9:41</time>
-                <div className={styles.HeaderSignalPlaceholder}>
-                    <div className={styles.HeaderSignals}>
-                        <img className={styles.HeaderSignalMobile} src="/mobile.png" alt="モバイル通信状態" />
-                    </div>
-                    <div className={styles.HeaderSignals}>
-                        <img className={styles.HeaderSignalWifi} src="/wifi.png" alt="wifi通信状態" />
-                    </div>
-                    <div className={styles.HeaderSignals}>
-                        <img className={styles.HeaderSignalBattery} src="/batery.png" alt="バッテリ残量" />
-                    </div>
-                </div>
-            </div>
             <div className={styles.HeaderTitleHolder}>
                 <div className={styles.HeaderTitles}>
                     {imgSrc && <img className={styles.HeaderIcon} src={imgSrc} alt={pageTitle} />}
